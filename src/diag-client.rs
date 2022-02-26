81e1d55108968ba23b4bc0e68786ac46f9722fe2
Brandon Ros <brandonros1@gmail.com>
2022-02-26 11:55:42 -0500

    loop

--- src/diag-client.rs
+++ src/diag-client.rs
@@ -13,22 +13,26 @@ use std::error::Error;
 async fn main() -> Result<(), Box<dyn Error>> {
   std::env::set_var("RUST_LOG", "debug");
   env_logger::init();
-  // start timer
-  let start_time = std::time::Instant::now();
-  // fire request
-  let server_ip = String::from("127.0.0.1");
-  let server_port = 3000;
-  let request_url = format!("http://{}:{}/ping", server_ip, server_port);
-  let client = Client::new();
-  let request = Request::builder()
-    .method("GET")
-    .uri(Uri::from_str(&request_url).unwrap())
-    .body(Body::empty())
-    .unwrap();
-  println!("requesting to diag-tunnel-server from diag-client {}", request_url);
-  let response = client.request(request).await.unwrap();
-  println!("got response from diag-tunnel-server {}", response.status());
-  let response_body_bytes = hyper::body::to_bytes(response.into_body()).await.unwrap();
-  println!("{:?}ms {:?}", start_time.elapsed().as_millis(), response_body_bytes);
+  loop {
+    // start timer
+    let start_time = std::time::Instant::now();
+    // fire request
+    let server_ip = String::from("127.0.0.1");
+    let server_port = 3000;
+    let request_url = format!("http://{}:{}/ping", server_ip, server_port);
+    let client = Client::new();
+    let request = Request::builder()
+      .method("GET")
+      .uri(Uri::from_str(&request_url).unwrap())
+      .body(Body::empty())
+      .unwrap();
+    println!("requesting to diag-tunnel-server from diag-client {}", request_url);
+    let response = client.request(request).await.unwrap();
+    println!("got response from diag-tunnel-server {}", response.status());
+    let response_body_bytes = hyper::body::to_bytes(response.into_body()).await.unwrap();
+    println!("{:?}ms {:?}", start_time.elapsed().as_millis(), response_body_bytes);
+    // sleep
+    std::thread::sleep(std::time::Duration::from_millis(100));
+  }
   Ok(())
 }
